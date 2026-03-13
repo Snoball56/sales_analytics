403a4335bbd238e098cfe1eb42c2618262d6df3e
Snoball56 <d.gauch@gmx.net>
2026-03-13 10:27:35 +0100

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -49,41 +49,12 @@ SELECT * FROM analytics.dim_product LIMIT 5;
 SELECT * FROM analytics.dim_date LIMIT 5;
 SELECT * FROM analytics.fact_sales LIMIT 5;
 
-SELECT COUNT(*) FROM analytics.dim_customer; --96000
-SELECT COUNT(*) FROM analytics.dim_product; --33000
-SELECT COUNT(*) FROM analytics.dim_date; --634
-SELECT COUNT(*) FROM analytics.fact_sales; --113300
-
 --auf Duplikate prüfen
 SELECT customer_unique_id, COUNT(*)
 FROM dim_customer
 GROUP BY customer_unique_id
 HAVING COUNT(*) > 1;
 
-TRUNCATE TABLE analytics.dim_customer RESTART IDENTITY CASCADE;
-TRUNCATE TABLE analytics.dim_product RESTART IDENTITY CASCADE;
-TRUNCATE TABLE analytics.dim_date RESTART IDENTITY CASCADE;
-TRUNCATE TABLE analytics.fact_sales RESTART IDENTITY CASCADE;
-
-TRUNCATE TABLE analytics.dim_date RESTART IDENTITY CASCADE;
 
 
-SELECT d::date
-FROM generate_series(
-    (SELECT MIN(date) FROM analytics.dim_date),
-    (SELECT MAX(date) FROM analytics.dim_date),
-    '1 day'
-) d
-LEFT JOIN analytics.dim_date dd
-ON d = dd.date
-WHERE dd.date IS NULL;
-
-SELECT table_schema, table_name
-FROM information_schema.tables
-WHERE table_name = 'dim_date';
-
-DROP TABLE public.dim_date;
-DROP TABLE public.dim_customer;
-DROP TABLE public.dim_product;
-DROP TABLE public.fact_sales;
 
